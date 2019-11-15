723e525cb0471a2e6232282e02129ec2cf5965ca
Julien Micheletti <julien.micheletti1@gmail.com>
2019-11-15 23:57:49 +0100

    ajout environnement back + service de questionnaire

---
+++ src/app/interfaces/questionnaire.ts
@@ -0,0 +1,16 @@
+export interface Questionnaire {
+  id?: string;
+  level: string;
+  category: string;
+  questionnaire: Question[];
+}
+
+export interface Question {
+  title: string;
+  choices: Choice[];
+  response: string;
+}
+
+export interface Choice {
+  choice: string;
+}
