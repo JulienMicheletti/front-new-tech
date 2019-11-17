568af07f308b12221931b742dccea96bd2abad9b
Julien Micheletti <julien.micheletti1@gmail.com>
2019-11-17 10:47:56 +0100

    design des quiz

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {MatIconRegistry} from "@angular/material/icon";
+import {DomSanitizer} from "@angular/platform-browser";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'front-new-tech';
+
+  constructor(private _matIconRegistry: MatIconRegistry, private _domSanitizer: DomSanitizer) {
+  }
+
+    ngOnInit() {
+      this._matIconRegistry.addSvgIcon('icon-delete', this._domSanitizer.bypassSecurityTrustResourceUrl('/assets/images/icon-delete.svg'));
+      this._matIconRegistry.addSvgIcon('icon-edit', this._domSanitizer.bypassSecurityTrustResourceUrl('/assets/images/icon-edit.svg'));
+    }
 }
